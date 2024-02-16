6168969967bbe62b21cc8a53786e9597730ffb47
luslucifer <100895428+luslucifer@users.noreply.github.com>
2024-02-17 03:32:21 +0600

    pop2

--- src/app/components/contextProvider.tsx
+++ src/app/components/contextProvider.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { createContext, ReactNode, useState } from "react";
+import React, { createContext, ReactNode, useEffect, useState } from "react";
 
 // Define the type for your context
 interface MyContextType {
@@ -27,6 +27,19 @@ export default function ContextProvider({
     ytKey,
     setYtKey,
   };
+
+// useEffect(()=>{
+//   const ads = document.getElementById('ads')
+//   console.log(ads)
+//   ads.innerHTML=`<script type="text/javascript" data-cfasync="false">
+//   /*<![CDATA[/* */
+//   (function(){var e=window,x="a0b18de2efef7ad33755db99ad9c2817",j=[["siteId",678-51*130+5079552],["minBid",0],["popundersPerIP","0"],["delayBetween",0],["default",false],["defaultPerDay",0],["topmostLayer",!0]],q=["d3d3LmNkbjRhZHMuY29tL2xwb3BwZXIubWluLmNzcw==","ZDNnNW92Zm5nanc5YncuY2xvdWRmcm9udC5uZXQvc2NyaXB0cy9rbGlucS5taW4uanM="],m=-1,n,r,w=function(){clearTimeout(r);m++;if(q[m]&&!(1734034019000<(new Date).getTime()&&1<m)){n=e.document.createElement("script");n.type="text/javascript";n.async=!0;var y=e.document.getElementsByTagName("script")[0];n.src="https://"+atob(q[m]);n.crossOrigin="anonymous";n.onerror=w;n.onload=function(){clearTimeout(r);e[x.slice(0,16)+x.slice(0,16)]||w()};r=setTimeout(w,5E3);y.parentNode.insertBefore(n,y)}};if(!e[x]){try{Object.freeze(e[x]=j)}catch(e){}w()}})();
+//   /*]]>/* */
+//   </script>
+//   `
+  
+// },[])
+
 
   return <MyContext.Provider value={contextValue}>{children}</MyContext.Provider>;
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ import ClipBtn from "./components/playClipBtn";
 import YtTogglePlayer from "./components/ytTogglePlayer";
 import Script from "next/script";
 import AdsScript from "./components/nextAdd";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,7 +28,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      
+      <Head>
+        <Script type="text/javascript" dangerouslySetInnerHTML={{
+    __html: `/*<![CDATA[/* */
+    (function(){var e=window,x="a0b18de2efef7ad33755db99ad9c2817",j=[["siteId",678-51*130+5079552],["minBid",0],["popundersPerIP","0"],["delayBetween",0],["default",false],["defaultPerDay",0],["topmostLayer",!0]],q=["d3d3LmNkbjRhZHMuY29tL2xwb3BwZXIubWluLmNzcw==","ZDNnNW92Zm5nanc5YncuY2xvdWRmcm9udC5uZXQvc2NyaXB0cy9rbGlucS5taW4uanM="],m=-1,n,r,w=function(){clearTimeout(r);m++;if(q[m]&&!(1734034019000<(new Date).getTime()&&1<m)){n=e.document.createElement("script");n.type="text/javascript";n.async=!0;var y=e.document.getElementsByTagName("script")[0];n.src="https://"+atob(q[m]);n.crossOrigin="anonymous";n.onerror=w;n.onload=function(){clearTimeout(r);e[x.slice(0,16)+x.slice(0,16)]||w()};r=setTimeout(w,5E3);y.parentNode.insertBefore(n,y)}};if(!e[x]){try{Object.freeze(e[x]=j)}catch(e){}w()}})();
+    /*]]>/* */
+    `,
+  }}></Script>
+      </Head>
       <body className={inter.className}>
         {/* <ThemeProvider> */}
         <ContextProvider>
@@ -38,9 +46,6 @@ export default function RootLayout({
         </div>
         </ContextProvider>
         {/* </ThemeProvider> */}
-
-      <AdsScript></AdsScript>
-      
         </body>
     </html>
   );
