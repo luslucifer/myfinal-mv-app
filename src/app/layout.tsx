438ebf40fbba3a75b6b4ec72b5e5a6e0e7ee9b57
luslucifer <100895428+luslucifer@users.noreply.github.com>
2024-02-17 10:04:40 +0600

    popads successfully added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,14 +28,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <Script type="text/javascript" dangerouslySetInnerHTML={{
-    __html: `/*<![CDATA[/* */
-    (function(){var e=window,x="a0b18de2efef7ad33755db99ad9c2817",j=[["siteId",678-51*130+5079552],["minBid",0],["popundersPerIP","0"],["delayBetween",0],["default",false],["defaultPerDay",0],["topmostLayer",!0]],q=["d3d3LmNkbjRhZHMuY29tL2xwb3BwZXIubWluLmNzcw==","ZDNnNW92Zm5nanc5YncuY2xvdWRmcm9udC5uZXQvc2NyaXB0cy9rbGlucS5taW4uanM="],m=-1,n,r,w=function(){clearTimeout(r);m++;if(q[m]&&!(1734034019000<(new Date).getTime()&&1<m)){n=e.document.createElement("script");n.type="text/javascript";n.async=!0;var y=e.document.getElementsByTagName("script")[0];n.src="https://"+atob(q[m]);n.crossOrigin="anonymous";n.onerror=w;n.onload=function(){clearTimeout(r);e[x.slice(0,16)+x.slice(0,16)]||w()};r=setTimeout(w,5E3);y.parentNode.insertBefore(n,y)}};if(!e[x]){try{Object.freeze(e[x]=j)}catch(e){}w()}})();
-    /*]]>/* */
-    `,
-  }}></Script>
-      </Head>
+      <head>
+      <script  type="text/javascript" data-cfasync="false" dangerouslySetInnerHTML={{
+        __html:`/*<![CDATA[/* */
+        (function(){var e=window,x="a0b18de2efef7ad33755db99ad9c2817",j=[["siteId",678-51*130+5079552],["minBid",0],["popundersPerIP","0"],["delayBetween",0],["default",false],["defaultPerDay",0],["topmostLayer",!0]],q=["d3d3LmNkbjRhZHMuY29tL2xwb3BwZXIubWluLmNzcw==","ZDNnNW92Zm5nanc5YncuY2xvdWRmcm9udC5uZXQvc2NyaXB0cy9rbGlucS5taW4uanM="],m=-1,n,r,w=function(){clearTimeout(r);m++;if(q[m]&&!(1734034019000<(new Date).getTime()&&1<m)){n=e.document.createElement("script");n.type="text/javascript";n.async=!0;var y=e.document.getElementsByTagName("script")[0];n.src="https://"+atob(q[m]);n.crossOrigin="anonymous";n.onerror=w;n.onload=function(){clearTimeout(r);e[x.slice(0,16)+x.slice(0,16)]||w()};r=setTimeout(w,5E3);y.parentNode.insertBefore(n,y)}};if(!e[x]){try{Object.freeze(e[x]=j)}catch(e){}w()}})();
+        /*]]>/* */` }}>
+  </script>
+      </head>
       <body className={inter.className}>
         {/* <ThemeProvider> */}
         <ContextProvider>
