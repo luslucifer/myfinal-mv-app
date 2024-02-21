2aabbfd5177794a991d813dd7990619372908332
luslucifer <100895428+luslucifer@users.noreply.github.com>
2024-02-21 12:47:18 +0600

    tv and anime fixed

--- src/app/components/posterCard.tsx
+++ src/app/components/posterCard.tsx
@@ -16,10 +16,21 @@ interface PosterCard {
 }
 
 export default  function PosterCard(props: PosterCard) {
+
+  const animeGere = props.obj.genre_ids.includes(16)
+
+
   function difiningMovieTv(){
     if(props.isTv || props.obj.media_type=="tv"){
-      return 'tv'
+      if(animeGere){
+        return 'anime'
+      }
+      else{
+        return 'tv'
+      }
+
     }
+  
     else{
       return 'movie'
     }
@@ -32,14 +43,16 @@ export default  function PosterCard(props: PosterCard) {
     }
   }
   const vote_average = Math.round(obj.vote_average*10)
+
+  const title = props.obj.title != undefined ? props.obj.title : props.obj.name
   return (
     // <Link href={`/movie/${obj.title?.replace(/ /g, "-") + "-" + obj.id}`}>
 
       <Card sx={{minWidth:'12rem',maxWidth:'12rem'}}>
         <CardActionArea>
-        <Link href={`/${difiningMovieTv()}/${obj.title?.replace(/[ /]/g, "-")}-${obj.id}`}>
+        <Link href={`/${difiningMovieTv()}/${title?.replace(/[ /]/g, "-")}-${obj.id}`}>
           <Box sx={{position:'relative',aspectRatio:`${2000/3000 }` , width:'100%'}}>
-          <Image src={ `https://image.tmdb.org/t/p/original/${obj.poster_path}`} layout="fill" objectFit="cover" alt={`tmdb poster of ${props.obj.title||obj.name}`}></Image>
+          <Image src={ `https://image.tmdb.org/t/p/original/${obj.poster_path}`} layout="fill" objectFit="cover" alt={`tmdb poster of ${title}`}></Image>
           </Box>
     </Link>
           <CardContent>
@@ -48,14 +61,13 @@ export default  function PosterCard(props: PosterCard) {
             <ClipBtn obj={undefined} id={obj.id} variant="contained" color='secondary'></ClipBtn>
             </Stack>
             <Typography gutterBottom variant="body1" component="div" align="center">
-              {obj.title||obj.name}
+              {title}
             </Typography>
             <Typography gutterBottom variant="body2">
               {formatDateToWords(obj.release_date||obj.first_air_date)}
             </Typography>
           </CardContent>
         </CardActionArea>
-        
       </Card>
   );
 }
---
+++ src/app/components/test.mjs
@@ -0,0 +1,5 @@
+const arr = [1,2,3,4,5,6]
+
+const x = arr.includes(10)
+
+console.log(x)
