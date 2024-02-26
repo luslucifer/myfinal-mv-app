c8a7fa83ddd2adaed52833298e5af787bdc9b627
luslucifer <100895428+luslucifer@users.noreply.github.com>
2024-02-27 03:14:42 +0600

    some posters optimized

--- src/app/components/posterCard.tsx
+++ src/app/components/posterCard.tsx
@@ -1,73 +1,100 @@
-
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import { Box, Button, CardActionArea, CardActions, Stack } from "@mui/material";
+import { Box, CardActionArea, CardMedia, Stack } from "@mui/material";
 import { Movie, formatDateToWords } from "../data-storage/fuctions-data";
 import Link from "next/link";
 import CircularProgressbarComponent from "./circularProgressBar";
 import ClipBtn from "./playClipBtn";
-import Image from "next/image";
-
 
 interface PosterCard {
   obj: Movie;
-  isTv:boolean;
+  isTv: boolean;
 }
 
-export default  function PosterCard(props: PosterCard) {
+export default function PosterCard(props: PosterCard) {
+  const animeGere = props.obj.genre_ids.includes(16);
 
-  const animeGere = props.obj.genre_ids.includes(16)
-
-
-  function difiningMovieTv(){
-    if(props.isTv || props.obj.media_type=="tv"){
-      if(animeGere){
-        return 'anime'
-      }
-      else{
-        return 'tv'
-      }
-
-    }
-  
-    else{
-      return 'movie'
+  function definingMovieTv() {
+    if (props.isTv || props.obj.media_type == "tv") {
+      return animeGere ? "anime" : "tv";
+    } else {
+      return "movie";
     }
   }
-  const obj =  props.obj;
 
-  if(!props.isTv){
-    if (!obj.title || obj.title==undefined){
-      return null
+  const obj = props.obj;
+
+  if (!props.isTv) {
+    if (!obj.title || obj.title === undefined) {
+      return null;
     }
   }
-  const vote_average = Math.round(obj.vote_average*10)
+  const vote_average = Math.round(obj.vote_average * 10);
 
-  const title = props.obj.title != undefined ? props.obj.title : props.obj.name
+  const title =
+    props.obj.title !== undefined ? props.obj.title : props.obj.name;
+
   return (
-    // <Link href={`/movie/${obj.title?.replace(/ /g, "-") + "-" + obj.id}`}>
-
-      <Card sx={{minWidth:'12rem',maxWidth:'12rem'}}>
-        <CardActionArea>
-        <Link href={`/${difiningMovieTv()}/${title?.replace(/[ /]/g, "-")}-${obj.id}`}>
-          <Box sx={{position:'relative',aspectRatio:`${2000/3000 }` , width:'100%'}}>
-          <Image src={ `https://image.tmdb.org/t/p/original/${obj.poster_path}`} layout="fill" objectFit="cover" alt={`tmdb poster of ${title}`}></Image>
+    <Card sx={{ minWidth: "12rem", maxWidth: "12rem" }}>
+      <CardActionArea>
+        <Link
+          href={`/${definingMovieTv()}/${title?.replace(/[ /]/g, "-")}-${
+            obj.id
+          }`}
+        >
+          <Box
+            sx={{
+              position: "relative",
+              aspectRatio: `${2000 / 3000}`,
+              width: "100%",
+              backgroundImage:`https://image.tmdb.org/t/p/w45/${obj.poster_path}`
+            }}
+          >
+            <CardMedia
+              component="img"
+              src={`https://image.tmdb.org/t/p/original/${obj.poster_path}`}
+              srcSet={`
+    https://image.tmdb.org/t/p/w92/${obj.poster_path} 92w,
+    https://image.tmdb.org/t/p/w154/${obj.poster_path} 154w,
+    https://image.tmdb.org/t/p/w185/${obj.poster_path} 185w,
+    https://image.tmdb.org/t/p/w342/${obj.poster_path} 342w,
+    https://image.tmdb.org/t/p/w500/${obj.poster_path} 500w,
+    https://image.tmdb.org/t/p/w780/${obj.poster_path} 780w,
+    https://image.tmdb.org/t/p/original/${obj.poster_path} 1080w
+  `}
+              loading="lazy"
+              alt={`tmdb poster of ${title}`}
+              // style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              sx={{ heught: "100%", width: "100%", objectFit: "cover" }}
+            />
           </Box>
-    </Link>
-          <CardContent>
-            <Stack flexDirection={'row'} justifyContent={"space-between"} >
-            <CircularProgressbarComponent number={vote_average}></CircularProgressbarComponent>
-            <ClipBtn obj={undefined} id={obj.id} variant="contained" color='secondary'></ClipBtn>
-            </Stack>
-            <Typography gutterBottom variant="body1" component="div" align="center">
-              {title}
-            </Typography>
-            <Typography gutterBottom variant="body2">
-              {formatDateToWords(obj.release_date||obj.first_air_date)}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+        </Link>
+        <CardContent>
+          <Stack flexDirection={"row"} justifyContent={"space-between"}>
+            <CircularProgressbarComponent
+              number={vote_average}
+            ></CircularProgressbarComponent>
+            <ClipBtn
+              obj={undefined}
+              id={obj.id}
+              variant="contained"
+              color="secondary"
+            ></ClipBtn>
+          </Stack>
+          <Typography
+            gutterBottom
+            variant="body1"
+            component="div"
+            align="center"
+          >
+            {title}
+          </Typography>
+          <Typography gutterBottom variant="body2">
+            {formatDateToWords(obj.release_date || obj.first_air_date)}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
 }
